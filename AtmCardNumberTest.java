6a13b3ba7752b3c37f61a55e999962051f9111c9
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-05-30 15:57:09 +0100

    ATM card validation test

--- AtmCardNumberTest.java
+++ AtmCardNumberTest.java
@@ -8,10 +8,28 @@ public class AtmCardNumberTest{
 	@Test
 	public void cardNumberCheckForAtmCards(){
 
-	int[] check = {3,5,6,7,4,6,4,3,3,5,6,5,7,4,6};
-
-	String expected = "American Express";
-
+	int[] check = {3,5,6,7,4,6,4,5,6,5,7,4,6};
+	String expected = "Invalid length";
+	
 	assertEquals(expected, quiz.cardNumberIn(check));
 	}
+
+	@Test
+	public void checkForVisaAtmCards(){
+
+	int[] actual = {4,3,5,6,7,4,6,4,5,6,5,7,4,6,5,7};
+	String feedBack = "Visa";
+
+	assertEquals(feedBack, quiz.cardNumberIn(actual));
+	}
+
+	@Test
+	public void checkForMastercardAtmCards(){
+
+	int[] actual = {5,3,5,6,7,4,6,4,5,6,5,7,4,6,5,7};
+	String feedBack = "Mastercard";
+
+	assertEquals(feedBack, quiz.cardNumberIn(actual));
+	}
+
 }
